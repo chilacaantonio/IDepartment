73642fa54405ac9a2463d1a022df93628cf0ad77
Antonio chilaca <dragonpiecez@hotmail.com>
2016-08-30 15:02:01 -0500

    confirmtion_token
    sdf

--- db/schema.rb
+++ db/schema.rb
@@ -45,8 +45,10 @@ ActiveRecord::Schema.define(version: 20160825235021) do
     t.string   "materno"
     t.string   "escuela"
     t.datetime "edad"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
 end
