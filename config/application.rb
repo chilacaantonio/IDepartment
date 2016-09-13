0a326fb1423d0c745bef9e867698b4d20af3d06b
Antonio chilaca <dragonpiecez@hotmail.com>
2016-09-12 19:54:52 -0500

    time

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Intelligence
   class Application < Rails::Application
+    config.time_zone = 'Guadalajara'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
